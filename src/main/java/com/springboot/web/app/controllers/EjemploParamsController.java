2d10827c1083feb283b863ce2975c4f6a447bf24
Luis B <luis.beroiza@gmail.com>
2023-05-11 14:59:08 +0200

    Probando Urls Dinamicas y paso de parametros GET con diferentes tipos
    Se pasaron mediante Get, parametros String e Integer

--- src/main/java/com/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/springboot/web/app/controllers/EjemploParamsController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+
+
+
+
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
@@ -20,4 +26,31 @@ public class EjemploParamsController {
     model.addAttribute("textoIngresado", String.format("El Texto ingresado fue: '%s'", texto));
     return "params/string";
   }
+
+
+  @GetMapping("/mix-params")
+  public String paramString(@RequestParam String texto, @RequestParam Integer number, Model model) {
+    model.addAttribute("textoIngresado", String.format("El Texto ingresado fue: '%s' y el numero fué '%d'", texto, number));
+    return "params/string";
+  }
+
+  //  Forma Antigua de trabajar con Request Params
+  @GetMapping("/mix-params-request")
+  public String paramString(HttpServletRequest request, Model model) {
+    String texto = request.getParameter("texto");
+    Integer number = null; 
+    try {
+      number = Integer.parseInt(request.getParameter("number"));
+    }catch(NumberFormatException nex) {
+      System.out.println("No se pudo convertir a Integer el valor de number: " + request.getParameter("number"));
+      number = 0;
+    }catch(Exception ex) {
+      System.err.println(ex);
+      number = -1;
+    }
+    
+    model.addAttribute("textoIngresado", String.format("Usando HttpServletRequest - El Texto ingresado fue: '%s' y el numero fué '%d'", texto, number));
+    return "params/string";
+  }
+
 }
---
+++ src/main/java/com/springboot/web/app/controllers/EjemploPathVariableController.java
@@ -0,0 +1,31 @@
+package com.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/path_variables")
+public class EjemploPathVariableController {
+
+  @GetMapping(value = {"", "/"})
+  public String index(Model model) {
+    model.addAttribute("title", "Usando Urls dinamicas");
+    model.addAttribute("h1", "Probando Urls dinamicas con Spring Boot");
+    return "path_variables/index";
+  }
+
+  //  La parte variable de la URL debe ir entre {} y el nombre que se puso allí
+  //  debe ser el mismo que el que se usa en los argumentos de la funcion
+  //  como en este ejemplo el nombre de la variable de argumento es "texto"
+  //  se usó dentro de @PathVariable el argumento (name="texto_variable")
+  @GetMapping("/user/{texto_variable}")
+  public String showUser(@PathVariable(name="texto_variable") String texto, Model model) {
+    model.addAttribute("textoIngresado", String.format("Pagina del usuario con id: '%s'", texto));
+    return "path_variables/show";
+  }
+
+}
